9e693b9a1d5935b176d047d9373d7e4e6e6af13b
DESKTOP-4EJAK4E\mingy <mingyeonging1203@gmail.com>
2022-05-30 22:59:27 +0900

    [BOJ] 세수정렬 / 5분
    - 완전 무식한 방법으로 성공

---
+++ week01/BOJ_2752.java
@@ -0,0 +1,41 @@
+package week01;
+
+import java.util.Scanner;
+
+public class BOJ_2752 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        if (a < b && a < c) {
+            System.out.println(a);
+            if (b < c) {
+                System.out.println(b);
+                System.out.println(c);
+            } else {
+                System.out.println(c);
+                System.out.println(b);
+            }
+        } else if (b < a && b < c) {
+            System.out.println(b);
+            if (a < c) {
+                System.out.println(a);
+                System.out.println(c);
+            } else {
+                System.out.println(c);
+                System.out.println(a);
+            }
+        } else {
+            System.out.println(c);
+            if (a < b) {
+                System.out.println(a);
+                System.out.println(b);
+            } else {
+                System.out.println(b);
+                System.out.println(a);
+            }
+        }
+    }
+}
