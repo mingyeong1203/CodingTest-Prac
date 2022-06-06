e3ff5d99cbaf24fd54f515b91850e57d8d3d7f10
DESKTOP-4EJAK4E\mingy <mingyeonging1203@gmail.com>
2022-06-06 22:53:19 +0900

    [BOJ] 숫자의 개수 / 1시간

--- week01/BOI_2577.java
+++ week01/BOI_2577.java
@@ -1,2 +1,24 @@
-package week01;public class BOI_2577 {
+package week01;
+
+import java.util.Scanner;
+
+public class BOI_2577 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int result = a * b * c;
+        String str = Integer.toString(result);
+
+        for(int i = 0; i<10; i++){
+            int count = 0;
+            for(int j = 0; j < str.length(); j++) {
+                if ((str.charAt(j) -'0')==i){
+                    count++;
+                }
+            }
+            System.out.println(count);
+        }
+    }
 }
